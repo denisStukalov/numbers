97752ebf02a74c0a6ee875e8d6743797df855a2a
Denis Stukalov <denisstukalov@gmail.com>
2020-09-04 01:23:04 +0300

    midleware thunk is added

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,12 +1,14 @@
-import { createStore, compose } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 import createRootReducer from './reducers'
 
+const middleWare = [thunk]
 const initState = {}
 
 const composeFunc =
   process.env.NODE_ENV === 'development' ? composeWithDevTools : compose
-const composedEnchanters = composeFunc()
+const composedEnchanters = composeFunc(applyMiddleware(...middleWare))
 const store = createStore(createRootReducer(), initState, composedEnchanters)
 
 export default store
